5b0a1a968ebbf7cd965cf25301a0abd9b829d6b4
WalterRombo <toby_gamlen@hotmail.co.uk>
2026-05-13 22:25:34 +0100

    Update sw.js
    Updated service worker to correct it blocking updates:
    
    line 4:
    
    const CACHE_NAME = 'parabola-v1'; 
    
    editted to:
    
    const CACHE_NAME = 'parabola-v2';

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Parabola Service Worker
 // Caches all app files so it works fully offline after first load
 
-const CACHE_NAME = 'parabola-v1';
+const CACHE_NAME = 'parabola-v2';
 
 const ASSETS_TO_CACHE = [
   './',
